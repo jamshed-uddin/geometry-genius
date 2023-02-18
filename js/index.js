d01a52f058923b8864ac998abccbd436ab514160
Jami <jamsheduddin03@gmail.com>
2023-02-18 23:25:50 +0600

    another functin called and overall check

--- js/index.js
+++ js/index.js
@@ -1,46 +1,25 @@
 //function funcToCalc(shapeBtnId,clickEvent,shapeNameId,angle1InputId,angle2InputId,valueForHalfOrPhiOrOne)
 
-funcToCalc(
-  "triangle-btn",
+funcToCalc("triangle-btn","triangle","triangle-b","triangle-h",0.5);
 
-  "triangle",
-  "triangle-b",
-  "triangle-h",
-  0.5
-);
 
-funcToCalc(
-  "rectangle-btn",
+funcToCalc("rectangle-btn","rectangle","ractangle-w","ractangle-l",1);
 
-  "rectangle",
-  "ractangle-w",
-  "ractangle-l",
-  1
-);
 
-funcToCalc(
-  "parallelogram-btn",
+funcToCalc("parallelogram-btn","parallelogram","parallelogram-b","parallelogram-h",1);
 
-  "parallelogram",
-  "parallelogram-b",
-  "parallelogram-h",
-  1
-);
 
 funcToCalc("rhombus-btn", "rhombus", "rhombus-b", "rhombus-h", 0.5);
 
-funcToCalc(
-  "pentagon-btn",
 
-  "pentagon",
-  "pentagon-b",
-  "pentagon-h",
-  0.5
-);
+funcToCalc("pentagon-btn","pentagon","pentagon-b","pentagon-h",0.5);
+
+
+funcToCalc("ellipse-btn","ellipse","ellipse-b","ellipse-h",3.1416);
+
 
 // function to redirect another page
 document
-  .getElementById("homeToBlogs-btn")
-  .addEventListener("click", function () {
+  .getElementById("homeToBlogs-btn").addEventListener("click", function () {
     location.href = "blogs.html";
   });
